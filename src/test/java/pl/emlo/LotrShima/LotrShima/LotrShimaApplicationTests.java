82c12674286bb156e323a727785bd455f1f0cf62
Emlo675 <Emil.Burdach@gmail.com>
2021-10-02 21:10:25 +0200

    Initial commit, starting Project with Spring, Maven

---
+++ src/test/java/pl/emlo/LotrShima/LotrShima/LotrShimaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.emlo.LotrShima.LotrShima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotrShimaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
