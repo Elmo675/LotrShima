388a3ed2f44466964168ae547b8e1f08cb768aa6
Emlo675 <Emil.Burdach@gmail.com>
2021-10-02 21:48:38 +0200

    -I Use-Case Prepered for 1st Stage of The game

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LotrShima
+
+# USE - Case for App
+https://lucid.app/documents/view/b831874f-6093-4a21-8c9b-6495de82fb7b
--- src/main/java/pl/emlo/LotrShima/LotrShima/LotrShimaApplication.java
+++ src/main/java/pl/emlo/LotrShima/LotrShima/LotrShimaApplication.java
@@ -1,13 +1,31 @@
 package pl.emlo.LotrShima.LotrShima;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LotrShimaApplication {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LotrShimaApplication.class, args);
-	}
+@SpringBootApplication
+public class LotrShimaApplication implements CommandLineRunner {
+    private static Logger LOG = LoggerFactory
+            .getLogger(LotrShimaApplication.class);
+
+    public static void main(String[] args) {
+        LOG.info("STARTING THE APPLICATION ffffffffffffffffuuuuuuuuuuuuuuuuuu");
+        SpringApplication.run(LotrShimaApplication.class, args);
+        LOG.info("APPLICATION FINISHED");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+        Scanner scanner = new Scanner(System.in);
+        String x;
+        x = scanner.nextLine();
+        LOG.info(x);
+    }
 
 }
