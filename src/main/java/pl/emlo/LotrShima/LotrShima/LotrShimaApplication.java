d9a917c6bd0ee4cd918de40d380c81ec0142e87f
Emlo675 <Emil.Burdach@gmail.com>
2021-10-03 14:54:58 +0200

    EB/FEAT/Item type added  enum

--- src/main/java/pl/emlo/LotrShima/LotrShima/LotrShimaApplication.java
+++ src/main/java/pl/emlo/LotrShima/LotrShima/LotrShimaApplication.java
@@ -1,7 +1,5 @@
 package pl.emlo.LotrShima.LotrShima;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,22 +8,19 @@ import java.util.Scanner;
 
 @SpringBootApplication
 public class LotrShimaApplication implements CommandLineRunner {
-    private static Logger LOG = LoggerFactory
-            .getLogger(LotrShimaApplication.class);
+    private static void print(String message) {
+        System.out.println(message);
+    }
 
     public static void main(String[] args) {
-        LOG.info("STARTING THE APPLICATION ffffffffffffffffuuuuuuuuuuuuuuuuuu");
+        print("APPLICATION START");
         SpringApplication.run(LotrShimaApplication.class, args);
-        LOG.info("APPLICATION FINISHED");
+        print("APPLICATION FINISHED");
     }
 
     @Override
     public void run(String... args) {
-        LOG.info("EXECUTING : command line runner");
-        Scanner scanner = new Scanner(System.in);
-        String x;
-        x = scanner.nextLine();
-        LOG.info(x);
+        print("cos robimy w grze");
     }
 
 }
